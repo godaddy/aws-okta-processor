e61f465d807674e164261e21114ef0a99a909067
Loren Gordon <lorengordon@users.noreply.github.com>
2019-10-01 15:27:06 -0700

    Adds test for totp factor

--- tests/core/test_okta.py
+++ tests/core/test_okta.py
@@ -1,7 +1,8 @@
 from test_base import TestBase
 from test_base import SESSION_RESPONSE
 from test_base import AUTH_TOKEN_RESPONSE
-from test_base import AUTH_MFA_RESPONSE
+from test_base import AUTH_MFA_PUSH_RESPONSE
+from test_base import AUTH_MFA_TOTP_RESPONSE
 from test_base import MFA_WAITING_RESPONSE
 from test_base import APPLICATIONS_RESPONSE
 from test_base import SAML_RESPONSE
@@ -201,7 +202,7 @@ class TestOkta(TestBase):
     @patch('aws_okta_processor.core.okta.os.makedirs')
     @patch('aws_okta_processor.core.okta.print_tty')
     @responses.activate
-    def test_okta_mfa_challenge(
+    def test_okta_mfa_push_challenge(
             self,
             mock_print_tty,
             mock_makedirs,
@@ -211,7 +212,7 @@ class TestOkta(TestBase):
         responses.add(
             responses.POST,
             'https://organization.okta.com/api/v1/authn',
-            json=json.loads(AUTH_MFA_RESPONSE)
+            json=json.loads(AUTH_MFA_PUSH_RESPONSE)
         )
 
         responses.add(
@@ -223,6 +224,50 @@ class TestOkta(TestBase):
         responses.add(
             responses.POST,
             'https://organization.okta.com/api/v1/authn/factors/id/lifecycle/activate/poll',
+            json=json.loads(AUTH_TOKEN_RESPONSE)
+        )
+
+        responses.add(
+            responses.POST,
+            'https://organization.okta.com/api/v1/sessions',
+            json=json.loads(SESSION_RESPONSE)
+        )
+
+        okta = Okta(
+            user_name="user_name",
+            user_pass="user_pass",
+            organization="organization.okta.com"
+        )
+
+        self.assertEqual(okta.okta_single_use_token, "single_use_token")
+        self.assertEqual(okta.organization, "organization.okta.com")
+        self.assertEqual(okta.okta_session_id, "session_token")
+
+    @patch('aws_okta_processor.core.okta.input')
+    @patch('aws_okta_processor.core.okta.os.chmod')
+    @patch('aws_okta_processor.core.okta.open')
+    @patch('aws_okta_processor.core.okta.os.makedirs')
+    @patch('aws_okta_processor.core.okta.print_tty')
+    @responses.activate
+    def test_okta_mfa_totp_challenge(
+            self,
+            mock_print_tty,
+            mock_makedirs,
+            mock_open,
+            mock_chmod,
+            mock_input
+    ):
+        mock_input.return_value = "123456"
+
+        responses.add(
+            responses.POST,
+            'https://organization.okta.com/api/v1/authn',
+            json=json.loads(AUTH_MFA_TOTP_RESPONSE)
+        )
+
+        responses.add(
+            responses.POST,
+            'https://organization.okta.com/api/v1/authn/factors/id/verify',
             json=json.loads(AUTH_TOKEN_RESPONSE)
         )
 
@@ -257,7 +302,7 @@ class TestOkta(TestBase):
         responses.add(
             responses.POST,
             'https://organization.okta.com/api/v1/authn',
-            json=json.loads(AUTH_MFA_RESPONSE)
+            json=json.loads(AUTH_MFA_PUSH_RESPONSE)
         )
 
         responses.add(
@@ -296,7 +341,7 @@ class TestOkta(TestBase):
         responses.add(
             responses.POST,
             'https://organization.okta.com/api/v1/authn',
-            json=json.loads(AUTH_MFA_RESPONSE)
+            json=json.loads(AUTH_MFA_PUSH_RESPONSE)
         )
 
         responses.add(

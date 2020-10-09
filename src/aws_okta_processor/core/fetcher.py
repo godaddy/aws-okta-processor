2084956b0f3742fad4ec90e36e963fa4d0e0a3f2
woz5999 <jeff.wozniak@umbralab.com>
2020-10-09 10:39:45 -0700

    support region flag

--- src/aws_okta_processor/core/fetcher.py
+++ src/aws_okta_processor/core/fetcher.py
@@ -50,7 +50,8 @@ class SAMLFetcher(CachedCredentialFetcher):
             'sts',
             aws_access_key_id='',
             aws_secret_access_key='',
-            aws_session_token=''
+            aws_session_token='',
+            region_name=self._configuration["AWS_OKTA_REGION"]
         )
 
         okta = Okta(

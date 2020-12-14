b08c4289ac3e5a0a26297b4503a5a9af87260260
Ravi Naik <ravinaik1312@gmail.com>
2020-12-14 10:19:00 -0700

    implement yubikey hardware token MFA (#33)

--- tests/core/test_okta.py
+++ tests/core/test_okta.py
@@ -4,6 +4,7 @@ from tests.test_base import AUTH_TOKEN_RESPONSE
 from tests.test_base import AUTH_MFA_PUSH_RESPONSE
 from tests.test_base import AUTH_MFA_TOTP_RESPONSE
 from tests.test_base import AUTH_MFA_MULTIPLE_RESPONSE
+from tests.test_base import AUTH_MFA_YUBICO_HARDWARE_RESPONSE
 from tests.test_base import MFA_WAITING_RESPONSE
 from tests.test_base import APPLICATIONS_RESPONSE
 from tests.test_base import SAML_RESPONSE
@@ -264,6 +265,50 @@ class TestOkta(TestBase):
             responses.POST,
             'https://organization.okta.com/api/v1/authn',
             json=json.loads(AUTH_MFA_TOTP_RESPONSE)
+        )
+
+        responses.add(
+            responses.POST,
+            'https://organization.okta.com/api/v1/authn/factors/id/verify',
+            json=json.loads(AUTH_TOKEN_RESPONSE)
+        )
+
+        responses.add(
+            responses.POST,
+            'https://organization.okta.com/api/v1/sessions',
+            json=json.loads(SESSION_RESPONSE)
+        )
+
+        okta = Okta(
+            user_name="user_name",
+            user_pass="user_pass",
+            organization="organization.okta.com"
+        )
+
+        self.assertEqual(okta.okta_single_use_token, "single_use_token")
+        self.assertEqual(okta.organization, "organization.okta.com")
+        self.assertEqual(okta.okta_session_id, "session_token")
+
+    @patch('aws_okta_processor.core.okta.input')
+    @patch('aws_okta_processor.core.okta.os.chmod')
+    @patch('aws_okta_processor.core.okta.open')
+    @patch('aws_okta_processor.core.okta.os.makedirs')
+    @patch('aws_okta_processor.core.okta.print_tty')
+    @responses.activate
+    def test_okta_mfa_hardware_token_challenge(
+            self,
+            mock_print_tty,
+            mock_makedirs,
+            mock_open,
+            mock_chmod,
+            mock_input
+    ):
+        mock_input.return_value = "123456"
+
+        responses.add(
+            responses.POST,
+            'https://organization.okta.com/api/v1/authn',
+            json=json.loads(AUTH_MFA_YUBICO_HARDWARE_RESPONSE)
         )
 
         responses.add(
